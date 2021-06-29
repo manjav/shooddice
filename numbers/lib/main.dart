085700abceee62b4d699836a407feba5fb31cc7f
Mansour <mansurjavadi@gmail.com>
2021-06-29 11:04:22 +0430

    Add  game template structure

---
+++ numbers/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:numbers/game.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue), home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: GameWidget(game: MyGame()));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
