839bf48dca327e4b555d3873077579663d77956c
Mansour <mansurjavadi@gmail.com>
2021-06-29 11:54:50 +0430

    Add core loop of game

--- numbers/lib/core/game.dart
+++ numbers/lib/core/game.dart
@@ -31,10 +31,76 @@ class MyGame extends BaseGame with TapDetector {
     super.render(canvas);
   }
 
+  void _spawn() {
+    // Check space is clean
+    if (_cells.existState(CellState.Flying)) return;
+    var reward = 0;
+    if (reward > 0) _numRewardCells++;
+    var cell = Cell(_nextCell.column, row, _nextCell.value, reward);
+    cell.x = cell.column * Cell.diameter + Cell.radius;
+    cell.y = _nextCell.y;
+    _cells.add(cell);
+    add(cell);
+
+    _nextCell.init(_nextCell.column, 0, Cell.getNextValue(), 0);
+
+  }
+
   void update(double dt) {
     super.update(dt);
+    if (!isPlaying) return;
+    if (_cells.last == null || _cells.last!.state != CellState.Flying) return;
+
+    // Check reach to target
+    if (_cells.last!.y < _cells.target!) {
+      _cells.last!.y += Cell.speed;
+      return;
+    }
+
+    // Change cell state
+    _fallAll();
   }
 
   void onTapDown(TapDownInfo info) {
+
+    if (!isPlaying) return;
+    var col = (info.eventPosition.global.x / Cell.diameter)
+        .clamp(0, Cells.width - 1)
+        .floor();
+    if (_nextCell.column != col) {
+      print("${_nextCell.column} changed to $col");
+      _nextCell.column = col;
+
+      if (_cells.last!.state == CellState.Flying) {
+        var row = _cells.length(_nextCell.column);
+        if (_cells.last! == _cells.get(_nextCell.column, row - 1)) --row;
+        _cells.translate(_cells.last!, _nextCell.column, row);
+        _cells.last!.x = _cells.last!.column * Cell.diameter + Cell.radius;
+      }
+    }
+    _fallAll();
+  }
+
+  void _fallAll() {
+    var numFallings = 0;
+    _cells.loop((i, j, c) {
+      c.state = CellState.Falling;
+      c.y = Cell.diameter * (Cells.height - c.row) + Cell.radius;
+      ++numFallings;
+    }, state: CellState.Flying);
+
+    if (numFallings > 0) {
+      _fell();
+    }
+  }
+
+
+  void _fell() {
+    _cells.loop((i, j, c) {
+      c.state = CellState.Fell;
+    }, state: CellState.Falling);
+
+    _spawn();
+  }
   }
   }
