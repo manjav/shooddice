ad76cbd9e490677455cd886f830958273dac0c88
Mansour <mansurjavadi@gmail.com>
2021-07-21 16:19:33 +0430

    Add reminder

--- numbers/lib/utils/notification.dart
+++ numbers/lib/utils/notification.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 class Notifier {
   static void init() async {
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -17,16 +20,37 @@ class Notifier {
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
         onSelectNotification: onSelectNotification);
+    tz.initializeTimeZones();
+    tz.setLocalLocation(tz.getLocation("Asia/Tehran"));
+
+    var title = "MergeNums";
+    var body = "You are really awesome!";
+    var times = [1, 3, 7];
+    await flutterLocalNotificationsPlugin.cancelAll();
+    for (var i = 0; i < times.length; i++) {
+      await flutterLocalNotificationsPlugin.zonedSchedule(
+          i,
+          title,
+          body,
+          tz.TZDateTime.now(tz.local)
+              .add(Duration(seconds: times[i] * 24 * 3600)),
+          const NotificationDetails(
+              android: AndroidNotificationDetails('your channel id',
+                  'your channel name', 'your channel description')),
+          androidAllowWhileIdle: true,
+          uiLocalNotificationDateInterpretation:
+              UILocalNotificationDateInterpretation.absoluteTime);
+    }
   }
 
   static Future<dynamic> onDidReceiveLocalNotification(
       int? id, String? title, String? body, String? payload) async {
-    debugPrint("title $title body $body payload: $payload");
+    debugPrint("=============  title $title body $body payload: $payload");
   }
 
   static Future<dynamic> onSelectNotification(String? payload) async {
     if (payload != null) {
-      debugPrint('notification payload: $payload');
+      debugPrint('("============= notification payload: $payload');
     }
   }
 }
