11cf6a850160387c10f357a7eb29c2f3d05f58ce
Mansour <mansurjavadi@gmail.com>
2021-08-10 16:35:13 +0430

    Add share ability

--- numbers/lib/overlays/stats.dart
+++ numbers/lib/overlays/stats.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:numbers/utils/prefs.dart';
+import 'package:numbers/utils/themes.dart';
 import 'package:numbers/utils/utils.dart';
+import 'package:numbers/widgets/buttons.dart';
 import 'package:numbers/widgets/widgets.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:share/share.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'all.dart';
 
@@ -13,38 +18,58 @@ class StatsOverlay extends StatefulWidget {
 }
 
 class _StatsOverlayState extends State<StatsOverlay> {
+  var _screenshotController = ScreenshotController();
+  var shareMode = false;
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
     return Overlays.basic(context,
         title: "Stats",
-        height: 380,
+        height: 460.d,
         statsButton: SizedBox(),
-        content: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SVG.show("record", 24.d),
-            Text(" ${Pref.record.value.format()}",
-                style: theme.textTheme.headline5)
-          ]),
-          SizedBox(height: 6.d),
-          // Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          // SVG.show("record", 24.d),
-          Text("Games Played: ${Pref.playCount.value}",
-              style: theme.textTheme.headline6)
-          // ])
-          ,
-          SizedBox(
-            width: 240.d,
-            height: 280.d,
-            child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 3.d,
-                mainAxisSpacing: 2.d,
-                childAspectRatio: 2,
-                children:
-                    List.generate(8, (i) => _bigRecordItem(theme, 9 + i))),
-          ),
-        ]));
+        content: Screenshot(
+            controller: _screenshotController,
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                SVG.show("record", 24.d),
+                Text(" ${Pref.record.value.format()}",
+                    style: theme.textTheme.headline5)
+              ]),
+              SizedBox(height: 6.d),
+              // Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              // SVG.show("record", 24.d),
+              Text("Games Played: ${Pref.playCount.value}",
+                  style: theme.textTheme.headline6)
+              // ])
+              ,
+              SizedBox(
+                width: 240.d,
+                height: 280.d,
+                child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 3.d,
+                    mainAxisSpacing: 2.d,
+                    childAspectRatio: 2,
+                    children:
+                        List.generate(8, (i) => _bigRecordItem(theme, 9 + i))),
+              ),
+              SizedBox(height: 26.d),
+              shareMode
+                  ? Padding(
+                      padding: EdgeInsets.all(10.d),
+                      child: Text(
+                          "This is my record. Are you ready to compete with me?",
+                          style: theme.textTheme.headline6))
+                  : BumpedButton(
+                      onTap: _share,
+                      colors: TColors.orange.value,
+                      content: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SVG.icon("8", theme),
+                            Text("Share", style: theme.textTheme.headline5)
+                          ]))
+            ])));
   }
 
   Widget _bigRecordItem(ThemeData theme, int i) {
@@ -58,5 +83,19 @@ class _StatsOverlayState extends State<StatsOverlay> {
         Text(" x ${Prefs.getBig(i)}", style: theme.textTheme.headline4)
       ],
     );
+  }
+
+  _share() async {
+    shareMode = true;
+    setState(() {});
+    final directory = await getApplicationDocumentsDirectory();
+    var imagePath = await _screenshotController.captureAndSave(directory.path);
+    if (imagePath != null)
+      await Share.shareFiles([imagePath],
+          text:
+              "https://play.google.com/apps/testing/game.block.puzzle.drop.the.number.merge",
+          subject: "Drop Number 2048 - Merge Block Puzzle");
+    shareMode = false;
+    setState(() {});
   }
 }
