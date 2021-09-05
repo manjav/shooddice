018b13b30de680f646fd0835361927f14e5e9f3f
Mansour <mansurjavadi@gmail.com>
2021-09-06 02:15:40 +0430

    Update theme based on screen size

--- numbers/lib/utils/themes.dart
+++ numbers/lib/utils/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:numbers/utils/utils.dart';
 
 enum TColors { black, white, whiteFlat, yellow, blue, orange, green }
 
@@ -51,19 +52,19 @@ class Themes {
 
   static ThemeData get darkData {
     var textTheme = TextTheme(
-        caption: TextStyle(color: TColors.white.value[2], fontSize: 16),
-        button: _style(TColors.black.value[0], 24, shadows: []),
-        bodyText1: _style(TColors.black.value[0], 22, shadows: []),
-        bodyText2: _style(TColors.black.value[0], 20, shadows: []),
-        subtitle1: _style(TColors.black.value[0], 16, shadows: []),
-        subtitle2: _style(TColors.black.value[0], 14, shadows: []),
-        headline1: _style(TColors.white.value[3], 56),
-        headline2: _style(TColors.white.value[3], 36),
-        headline3: _style(TColors.white.value[3], 30),
-        headline4: _style(TColors.white.value[3], 24),
-        headline5: _style(TColors.white.value[3], 20),
-        headline6: _style(TColors.white.value[3], 16),
-        overline: _style(TColors.white.value[3], 32, font: "icons"));
+        caption: TextStyle(color: TColors.white.value[2], fontSize: 16.d),
+        button: _style(TColors.black.value[0], 24.d, shadows: []),
+        bodyText1: _style(TColors.black.value[0], 22.d, shadows: []),
+        bodyText2: _style(TColors.black.value[0], 20.d, shadows: []),
+        subtitle1: _style(TColors.black.value[0], 16.d, shadows: []),
+        subtitle2: _style(TColors.black.value[0], 14.d, shadows: []),
+        headline1: _style(TColors.white.value[3], 56.d),
+        headline2: _style(TColors.white.value[3], 36.d),
+        headline3: _style(TColors.white.value[3], 30.d),
+        headline4: _style(TColors.white.value[3], 24.d),
+        headline5: _style(TColors.white.value[3], 20.d),
+        headline6: _style(TColors.white.value[3], 16.d),
+        overline: _style(TColors.white.value[3], 32.d, font: "icons"));
 
     // var iconTheme = IconThemeData(color: primaries[50]);
     return ThemeData(
