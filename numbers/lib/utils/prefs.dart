c449f3e723d4d2f1f3c68158bff697e84dac7ba2
Mansour <mansurjavadi@gmail.com>
2021-09-29 15:02:52 +0330

    Fix bbackup conflict

--- numbers/lib/utils/prefs.dart
+++ numbers/lib/utils/prefs.dart
@@ -7,18 +7,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Prefs {
   static int score = 0;
+  static bool allowsBackup = false;
   static SharedPreferences? _instance;
   static void init(Function onInit) {
     SharedPreferences.getInstance().then((SharedPreferences prefs) async {
       _instance = prefs;
       if (!prefs.containsKey("visitCount")) {
-        Pref.coin.set(500);
-        Pref.removeOne.set(3);
-        Pref.removeColor.set(3);
-        Pref.rateTarget.set(5);
-        _restore();
+        await _restore();
+        if (!prefs.containsKey("visitCount")) {
+          Pref.coin.set(500);
+          Pref.removeOne.set(3);
+          Pref.removeColor.set(3);
+          Pref.rateTarget.set(5);
+        }
       } else {
         _initPlayService();
+        allowsBackup = true;
       }
       Pref.visitCount.increase(1);
       onInit();
@@ -47,8 +51,10 @@ class Prefs {
         return; // default value when there is no save
       // var content =
       //     '{"playCount":32,"big_14":1,"removeColor":3,"big_11":1,"big_10":1,"big_13":1,"big_12":1,"visitCount":17,"record":148284,"rate":3,"rateTarget":35,"noAds":0,"big_9":2,"coin":2404,"tutorMode":1,"ratedBefore":0,"removeOne":3,"isMute":1,"isVibrateOff":1}';
+      debugPrint("o ${save.content!}");
       var data = json.decode(save.content!);
       for (var entry in data.entries) _instance!.setInt(entry.key, entry.value);
+      allowsBackup = true;
     } catch (e) {
       print(e.toString());
     }
@@ -57,17 +63,22 @@ class Prefs {
   static Timer? _wakeupTimer;
   static _backup() async {
     _wakeupTimer?.cancel();
-    _wakeupTimer = Timer(Duration(seconds: 5), () {
-      var keys = _instance!.getKeys();
-      var data = Map<String, int>();
-      for (var key in keys) {
-        var value = _instance!.getInt(key) ?? -1;
-        if (value > -1) data[key] = value;
+    if (!allowsBackup) return;
+    _wakeupTimer = Timer(Duration(seconds: 5), () async {
+      try {
+        var keys = _instance!.getKeys();
+        var data = Map<String, int>();
+        for (var key in keys) {
+          var value = _instance!.getInt(key) ?? -1;
+          if (value > -1) data[key] = value;
+        }
+        var saveData = json.encode(data);
+        debugPrint("s $saveData");
+        await PlayGames.saveSnapshot('prefs', saveData);
+        PlayGames.openSnapshot('prefs');
+      } catch (e) {
+        debugPrint(e.toString());
       }
-      var saveData = json.encode(data);
-      debugPrint(saveData);
-      PlayGames.saveSnapshot('prefs', saveData);
-      // PlayGames.openSnapshot('prefs');
     });
   }
 
@@ -75,8 +86,7 @@ class Prefs {
     SigninResult result = await PlayGames.signIn(scopeSnapshot: true);
     if (result.success) {
       // await PlayGames.setPopupOptions();
-      debugPrint(
-          "======${result.account!.displayName} ${result.account!.email}");
+      debugPrint("${result.account!.displayName} ${result.account!.email}");
     } else {
       debugPrint(result.message);
     }
