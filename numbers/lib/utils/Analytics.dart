f9172b97c8ffc73e4e7ec9ce21d8233471eb535e
Mansour <mansurjavadi@gmail.com>
2021-09-30 20:17:04 +0330

    Implement analytics init

--- numbers/lib/utils/Analytics.dart
+++ numbers/lib/utils/Analytics.dart
@@ -1,13 +1,30 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:gameanalytics_sdk/gameanalytics.dart';
+import 'dart:async';
 
 class Analytics {
   static late FirebaseAnalytics _firebaseAnalytics;
   static late FirebaseAnalyticsObserver _observer;
 
-
-  static void init(
-      FirebaseAnalytics analytics, FirebaseAnalyticsObserver observer) {
+  static Future<void> init(
+      FirebaseAnalytics analytics, FirebaseAnalyticsObserver observer) async {
     _firebaseAnalytics = analytics;
     _observer = observer;
+
+    GameAnalytics.setEnabledInfoLog(false);
+    GameAnalytics.setEnabledVerboseLog(false);
+
+    // GameAnalytics.configureAvailableCustomDimensions01(["ninja", "samurai"]);
+    // GameAnalytics.configureAvailableCustomDimensions02(["whale", "dolphin"]);
+    // GameAnalytics.configureAvailableCustomDimensions03(["horde", "alliance"]);
+    GameAnalytics.configureAvailableResourceCurrencies(["coin"]);
+    GameAnalytics.configureAvailableResourceItemTypes(
+        ["game", "confirm", "shop", "start"]);
+
+    GameAnalytics.configureAutoDetectAppVersion(true);
+    GameAnalytics.initialize("2c9380c96ef57f01f353906b341a21cc",
+        "275843fe2b762882e938a16d6b095d7661670ee9");
+  }
+
   }
