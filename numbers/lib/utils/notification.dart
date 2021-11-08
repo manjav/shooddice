9c5a3b824ba6f3c8f5d8888f4f3a3af199bc6d4d
Mansour <mansurjavadi@gmail.com>
2021-11-08 12:18:55 +0330

    Update messaging

--- numbers/lib/utils/notification.dart
+++ numbers/lib/utils/notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:numbers/utils/localization.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -7,16 +8,19 @@ class Notifier {
   static void init() async {
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
         FlutterLocalNotificationsPlugin();
-    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    AndroidInitializationSettings initializationSettingsAndroid =
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('app_icon');
     final IOSInitializationSettings initializationSettingsIOS =
         IOSInitializationSettings(
             onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    final MacOSInitializationSettings initializationSettingsMacOS =
+        MacOSInitializationSettings();
     final InitializationSettings initializationSettings =
         InitializationSettings(
             android: initializationSettingsAndroid,
-            iOS: initializationSettingsIOS);
+            iOS: initializationSettingsIOS,
+            macOS: initializationSettingsMacOS);
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
         onSelectNotification: onSelectNotification);
@@ -24,24 +28,23 @@ class Notifier {
     _reminder(flutterLocalNotificationsPlugin);
   }
 
-  static Future<void> _reminder(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+  static Future<void> _reminder(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
     tz.initializeTimeZones();
     tz.setLocalLocation(tz.getLocation("Asia/Tehran"));
 
-    var title = "MergeNums";
-    var body = "You are really awesome!";
     var times = [1, 3, 7];
+    var title = "app_title".l();
+    var body = "You are really awesome!";
+    const details = AndroidNotificationDetails('reminder', 'Reminder');
     await flutterLocalNotificationsPlugin.cancelAll();
     for (var i = 0; i < times.length; i++) {
       await flutterLocalNotificationsPlugin.zonedSchedule(
           i,
           title,
           body,
-          tz.TZDateTime.now(tz.local)
-              .add(Duration(seconds: times[i] * 24 * 3600)),
-          const NotificationDetails(
-              android: AndroidNotificationDetails(
-                  'reminder', 'Reminder', 'your channel description')),
+          tz.TZDateTime.now(tz.local).add(Duration(days: times[i])),
+          const NotificationDetails(android: details),
           androidAllowWhileIdle: true,
           uiLocalNotificationDateInterpretation:
               UILocalNotificationDateInterpretation.absoluteTime);
