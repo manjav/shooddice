9f4c057e2cf514c6d14481497270c73940536206
Mansour <mansurjavadi@gmail.com>
2021-11-29 19:45:43 +0330

    Redesign pauseDialog

--- numbers/lib/dialogs/pause.dart
+++ numbers/lib/dialogs/pause.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:numbers/utils/ads.dart';
 import 'package:numbers/utils/localization.dart';
 import 'package:numbers/utils/prefs.dart';
 import 'package:numbers/utils/themes.dart';
@@ -14,11 +13,8 @@ class PauseDialog extends AbstractDialog {
   PauseDialog()
       : super(
           DialogMode.pause,
-          height: 0,
-          hasChrome: false,
           title: "pause_l".l(),
           showCloseButton: false,
-          padding: EdgeInsets.all(0),
         );
   @override
   _PauseDialogState createState() => _PauseDialogState();
@@ -28,77 +24,83 @@ class _PauseDialogState extends AbstractDialogState<PauseDialog> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    widget.child =
-        Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      bannerAdsFactory(),
-      SizedBox(height: Ads.isReady(AdPlace.Banner) ? 16.d : 0),
-      SizedBox(
-          height: 76.d,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                BumpedButton(
-                    onTap: () => Navigator.of(context).pop("reset"),
-                    colors: TColors.green.value,
-                    cornerRadius: 16.d,
-                    content: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SVG.icon("5", theme),
-                          Text("pause_l".l(), style: theme.textTheme.headline5)
-                        ])),
-                BumpedButton(
-                    onTap: () => Navigator.of(context).pop("resume"),
-                    colors: TColors.blue.value,
-                    cornerRadius: 16.d,
-                    content: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SVG.icon("4", theme),
-                          Text("continue_l".l(),
-                              style: theme.textTheme.headline5)
-                        ]))
-              ])),
-      SizedBox(height: 12.d),
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        SizedBox(
-            width: 76.d,
-            height: 76.d,
-            child: BumpedButton(
-                onTap: () {
-                  Pref.isVibrateOff.set(Pref.isVibrateOff.value == 0 ? 1 : 0);
-                  setState(() {});
-                },
-                colors: TColors.orange.value,
-                cornerRadius: 16.d,
-                content: Center(
-                    child: SVG.icon("${Pref.isVibrateOff.value + 6}", theme,
-                        scale: 1.2)))),
-        SizedBox(width: 12.d),
-        SizedBox(
-            width: 76.d,
-            height: 76.d,
-            child: BumpedButton(
-                onTap: () {
-                  Pref.isMute.set(Pref.isMute.value == 0 ? 1 : 0);
-                  setState(() {});
-                },
-                colors: TColors.yellow.value,
-                cornerRadius: 16.d,
-                content: Center(
-                    child: SVG.icon("${Pref.isMute.value + 1}", theme,
-                        scale: 1.2))))
-      ])
-    ]);
-    return super.build(context);
-  }
 
-  @override
-  Widget bannerAdsFactory() {
-    if (!Ads.isReady(AdPlace.Banner)) return SizedBox();
-    return ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(16.d)),
-        child: Ads.getBanner());
+    var children = <Widget>[];
+    children.add(rankButtonFactory(theme));
+    children.add(statsButtonFactory(theme));
+    children.add(coinsButtonFactory(theme));
+
+    var rows = <Widget>[];
+    rows.add(headerFactory(theme, 300.d));
+    rows.add(SizedBox(
+        height: 76.d,
+        width: 300.d,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              BumpedButton(
+                  onTap: () => Navigator.of(context).pop("reset"),
+                  colors: TColors.green.value,
+                  cornerRadius: 16.d,
+                  content: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SVG.icon("5", theme),
+                        Text("pause_l".l(), style: theme.textTheme.headline5)
+                      ])),
+              BumpedButton(
+                  onTap: () => Navigator.of(context).pop("resume"),
+                  colors: TColors.blue.value,
+                  cornerRadius: 16.d,
+                  content: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SVG.icon("4", theme),
+                        Text("continue_l".l(), style: theme.textTheme.headline5)
+                      ]))
+            ])));
+    rows.add(SizedBox(height: 12.d));
+    rows.add(Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      SizedBox(
+          width: 76.d,
+          height: 76.d,
+          child: BumpedButton(
+              onTap: () {
+                Pref.isVibrateOff.set(Pref.isVibrateOff.value == 0 ? 1 : 0);
+                setState(() {});
+              },
+              colors: TColors.orange.value,
+              cornerRadius: 16.d,
+              content: Center(
+                  child: SVG.icon("${Pref.isVibrateOff.value + 6}", theme,
+                      scale: 1.2)))),
+      SizedBox(width: 12.d),
+      SizedBox(
+          width: 76.d,
+          height: 76.d,
+          child: BumpedButton(
+              onTap: () {
+                Pref.isMute.set(Pref.isMute.value == 0 ? 1 : 0);
+                setState(() {});
+              },
+              colors: TColors.yellow.value,
+              cornerRadius: 16.d,
+              content: Center(
+                  child:
+                      SVG.icon("${Pref.isMute.value + 1}", theme, scale: 1.2))))
+    ]));
+
+    children.add(
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: rows));
+    children.add(bannerAdsFactory());
+
+    return WillPopScope(
+        key: Key(widget.mode.name),
+        onWillPop: () async {
+          widget.onWillPop?.call();
+          return widget.closeOnBack ?? true;
+        },
+        child: Stack(alignment: Alignment.center, children: children));
   }
 }
