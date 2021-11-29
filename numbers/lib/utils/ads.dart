c735dca4e26627dc4435dde6147073c8cb28407e
Mansour <mansurjavadi@gmail.com>
2021-11-29 22:12:43 +0330

    Organize Ad states

--- numbers/lib/utils/ads.dart
+++ numbers/lib/utils/ads.dart
@@ -68,11 +68,15 @@ class Ads {
     var place = AdPlace.Banner;
     if (_banners.containsKey(type)) return _banners[type]!;
     var _listener = BannerAdListener(
-        onAdLoaded: (ad) => _adListener(place, AdState.Loaded, ad),
+        onAdLoaded: (ad) => _updateState(place, AdState.Loaded, ad),
         onAdFailedToLoad: (ad, error) {
-          _adListener(place, AdState.FailedLoad, ad, error);
+          _updateState(place, AdState.FailedLoad, ad, error);
           ad.dispose();
         },
+        onAdOpened: (ad) => _updateState(place, AdState.Clicked, ad),
+        onAdClosed: (ad) => _updateState(place, AdState.Closed, ad),
+        onAdImpression: (ad) => _updateState(place, AdState.Show, ad));
+    _updateState(place, AdState.Request);
     return _banners[type] = BannerAd(
         size: size ?? AdSize.largeBanner,
         adUnitId: place.id,
@@ -88,12 +92,12 @@ class Ads {
         request: _request,
         adLoadCallback:
             InterstitialAdLoadCallback(onAdLoaded: (InterstitialAd ad) {
-          _adListener(place, AdState.Loaded, ad);
+          _updateState(place, AdState.Loaded, ad);
           _interstitialAd = ad;
           _attemptsLoadForInterstitial = 0;
           _interstitialAd!.setImmersiveMode(true);
         }, onAdFailedToLoad: (LoadAdError error) {
-          _adListener(place, AdState.FailedLoad, null, error);
+          _updateState(place, AdState.FailedLoad, null, error);
           ++_attemptsLoadForInterstitial;
           _interstitialAd = null;
           if (_attemptsLoadForInterstitial <= maxFailedLoadAttempts)
@@ -110,9 +114,9 @@ class Ads {
             RewardedAdLoadCallback(onAdLoaded: (RewardedAd ad) {
           _rewardedAd = ad;
           _attemptsLoadForRewarded = 0;
-          _adListener(place, AdState.Loaded, ad);
+          _updateState(place, AdState.Loaded, ad);
         }, onAdFailedToLoad: (LoadAdError error) {
-          _adListener(place, AdState.FailedLoad, null, error);
+          _updateState(place, AdState.FailedLoad, null, error);
           _rewardedAd = null;
           ++_attemptsLoadForRewarded;
           if (_attemptsLoadForRewarded <= maxFailedLoadAttempts) _getRewarded();
@@ -136,22 +140,20 @@ class Ads {
     }
     var place = AdPlace.Interstitial;
     _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
-        onAdShowedFullScreenContent: (ad) =>
-            _adListener(place, AdState.Show, ad),
         onAdDismissedFullScreenContent: (InterstitialAd ad) {
-          _adListener(place, AdState.Dismissed, ad);
+          _updateState(place, AdState.Closed, ad);
           ad.dispose();
           _getInterstitial();
         },
         onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-          _adListener(place, AdState.FailedShow, ad, error);
+          _updateState(place, AdState.FailedShow, ad, error);
           ad.dispose();
           _getInterstitial();
         },
-        onAdImpression: (ad) => _adListener(place, AdState.Impression, ad));
-      _interstitialAd!.show();
+        onAdImpression: (ad) => _updateState(place, AdState.Show, ad));
+    _interstitialAd!.show();
     _interstitialAd = null;
-    //   await _waitForClose(AdPlace.Interstitial);
+    await _waitForClose(AdPlace.Interstitial);
   }
 
   static Future<RewardItem?> showRewarded() async {
@@ -162,27 +164,25 @@ class Ads {
     }
     var place = AdPlace.Rewarded;
     _rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
-        onAdShowedFullScreenContent: (ad) =>
-            _adListener(place, AdState.Show, ad),
         onAdDismissedFullScreenContent: (RewardedAd ad) {
-          _adListener(place, AdState.Dismissed, ad);
+          _updateState(place, AdState.Closed, ad);
           ad.dispose();
           _getRewarded();
         },
         onAdFailedToShowFullScreenContent: (RewardedAd ad, AdError error) {
-          _adListener(place, AdState.FailedShow, ad, error);
+          _updateState(place, AdState.FailedShow, ad, error);
           ad.dispose();
           _getRewarded();
         },
-        onAdImpression: (ad) => _adListener(place, AdState.Impression, ad));
+        onAdImpression: (ad) => _updateState(place, AdState.Show, ad));
     _rewardedAd!.setImmersiveMode(true);
     _rewardedAd!.show(
         onUserEarnedReward: (RewardedAd ad, RewardItem rewardItem) {
       reward = rewardItem;
-      return _adListener(place, AdState.Rewarded, ad);
+      return _updateState(place, AdState.RewardReceived, ad);
     });
+    await _waitForClose(AdPlace.Rewarded);
     _rewardedAd = null;
-      await _waitForClose(AdPlace.Rewarded);
     return reward;
   }
 
@@ -211,54 +211,45 @@ class Ads {
     return AdPlace.Rewarded;
   }
 
-  static _adListener(AdPlace place, AdState state, [Ad? ad, AdError? error]) {
-    _updateState(place, state);
-    var action = _getAction(state);
-    // if (action > 0) Analytics.ad(action, place.type, place.name, "admob");
-    debugPrint("Ads ==> $place ${state.toString()} $ad");
-  }
-
-  static void _updateState(AdPlace place, AdState state) {
+  static void _updateState(AdPlace place, AdState state,
+      [Ad? ad, AdError? error]) {
     _placements[place] = state;
     onUpdate?.call(place, state);
+    print("Ads ==> ${state.order}");
+    if (state.order > 0)
+      Analytics.ad(state.order, place.type, place.name, "admob");
+    debugPrint("Ads ==> $place ${state.toString()} ${error ?? ''}");
   }
 
   static _waitForClose(AdPlace adPlace) async {
     const d = Duration(milliseconds: 300);
-    while (_placements[adPlace] != AdState.Dismissed) await Future.delayed(d);
+    while (_placements[adPlace] != AdState.Closed) await Future.delayed(d);
   }
 
-  static int _getAction(AdState event) {
-    switch (event) {
-      case AdState.Loaded:
-        return GAAdAction.Loaded;
-      case AdState.Show:
-        return GAAdAction.Show;
-      case AdState.Rewarded:
-        return GAAdAction.RewardReceived;
-      case AdState.Clicked:
-        return GAAdAction.Clicked;
-      default:
-        return 0;
-    }
   }
 }
 
 enum AdState {
-  Clicked,
   Closed,
-  FailedLoad,
-  FailedShow,
-  Impression,
-  Loaded,
-  Opened,
-  Rewarded,
+  Clicked,
   Show,
-  Dismissed
+  FailedShow,
+  RewardReceived,
+  Request,
+  Loaded,
+  FailedLoad
 }
+
+extension AdExt on AdState {
+  int get order {
+    if (this == AdState.FailedLoad) return -1;
+    return index;
+  }
+}
+
 enum AdPlace { Rewarded, Interstitial, Banner }
 
-extension AdExt on AdPlace {
+extension AdPlaceExt on AdPlace {
   String get name {
     switch (this) {
       case AdPlace.Banner:
@@ -294,7 +285,7 @@ extension AdExt on AdPlace {
 
   int get threshold {
     if (this == AdPlace.Interstitial) return 7;
-    if (this == AdPlace.Banner) return 10;
+    // if (this == AdPlace.Banner) return 10;
     return 0;
   }
 }
