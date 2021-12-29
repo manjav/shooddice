e060f72fbee67585958f707dc3914d95287ce216
Mansour <mansurjavadi@gmail.com>
2021-12-30 00:41:38 +0330

    Design quests

--- numbers/lib/dialogs/quests.dart
+++ numbers/lib/dialogs/quests.dart
@@ -103,12 +103,92 @@ class Quests {
 }
 
 class Quest {
-  final String type;
-  final int min;
-  final int max;
-  final int reward;
-  int value;
-  Quest(this.type, this.min, this.value, this.max, this.reward);
+  int value = 0;
+  int level;
+  final QuestType type;
+  bool notified = false;
+
+  Quest(this.type, this.level, {int value = 0}) {
+    this.value = value;
+  }
+
+  bool get isDone => value >= max;
+
+  String get text => ("quest_" + type.name).l([max.toString()]);
+
+  void levelUp() {
+    Prefs.setInt("q_l_${type.name}", level + 1, true);
+    level++;
+    notified = false;
+  }
+
+  int get max {
+    return _getCheckpoint(type, level + 1);
+  }
+
+  int get min {
+    if (level == 0) return 0;
+    return _getCheckpoint(type, level);
+  }
+
+  static int _getCheckpoint(QuestType type, int stage) {
+    switch (type) {
+      case QuestType.b2048:
+        return pow(2, stage + 1).toInt();
+      case QuestType.merges:
+        return pow(2, stage + 6).toInt();
+      case QuestType.removeone:
+        return pow(2, stage).toInt();
+      case QuestType.video:
+        return pow(2, stage).toInt();
+    }
+  }
+
+  int get reward {
+    switch (type) {
+      case QuestType.b2048:
+        return (level + 1) * 1000;
+      case QuestType.merges:
+        return (level + 1) * 500;
+      case QuestType.removeone:
+        return pow(2, level).toInt() * 200;
+      case QuestType.video:
+        return (level + 1) * 300;
+    }
+  }
+}
+
+enum QuestType { merges, removeone, video, b2048 }
+
+extension QuestTypeExt on QuestType {
+  String get name {
+    switch (this) {
+      case QuestType.b2048:
+        return "b2048";
+      case QuestType.merges:
+        return "merges";
+      case QuestType.removeone:
+        return "removeone";
+      case QuestType.video:
+        return "video";
+    }
+  }
+
+  String get icon {
+    switch (this) {
+      case QuestType.b2048:
+        return "2048";
+      case QuestType.merges:
+        return "merge";
+      case QuestType.removeone:
+        return "remove-one";
+      case QuestType.video:
+        return "tv";
+    }
+  }
+
+  int get value => Prefs.getInt("q_$name");
+  int get level => Prefs.getInt("q_l_$name");
 }
 
 /*
