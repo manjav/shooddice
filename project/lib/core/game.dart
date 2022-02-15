6b9e3429d78ca306f20a3ea73d48a666e7359588
Mansour <mansurjavadi@gmail.com>
2022-02-15 19:42:10 +0330

    Add google play games configs

--- project/lib/core/game.dart
+++ project/lib/core/game.dart
@@ -9,6 +9,7 @@ import 'package:flame/palette.dart';
 import 'package:flame_svg/svg.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:games_services/games_services.dart';
 import 'package:project/animations/animate.dart';
 import 'package:project/core/achieves.dart';
 import 'package:project/core/cell.dart';
@@ -79,11 +80,11 @@ class MyGame extends FlameGame with TapDetector {
     var _new = Prefs.score += Cell.getScore(value);
     onGameEvent?.call(GameEvent.score, _new);
     if (Pref.record.value >= Prefs.score) return;
-    // GamesServices.submitScore(
-    //     score: Score(
-    //         androidLeaderboardID: 'CgkIw9yXzt4XEAIQAQ',
-    //         iOSLeaderboardID: 'ios_leaderboard_id',
-    //         value: Prefs.score));
+    GamesServices.submitScore(
+        score: Score(
+            androidLeaderboardID: 'CgkIhJHMyZAVEAIQAg',
+            iOSLeaderboardID: 'ios_leaderboard_id',
+            value: Prefs.score));
 
     Pref.record.set(Prefs.score);
     _newRecord = Prefs.score;
